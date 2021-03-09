bb0e09566b6d12fa87de4afea6bfb909b8b71ca2
Margyysik <71218459+Margyysik@users.noreply.github.com>
2021-03-10 01:04:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = bracketsConfig.map((item) => item.join('')); 
+
+  for(let i = 0; i < array.length; i++){
+
+    if (str.includes(array[i])) {
+      str = str.replace(array[i], '');
+      i = -1;
+    }
+  }
+  if (str == ''){
+  	return true
+  } else{
+  	return false
+  }
 }
